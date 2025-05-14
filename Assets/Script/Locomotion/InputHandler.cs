86bfddfaf12b2a94e34f50bc62fc2a5109d238f9
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-05-14 11:31:46 +0200

    [ADD] Player Movement

---
+++ Assets/Script/Locomotion/InputHandler.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private PlayerInput playerInput;
+    
+    [Space]
+    [Header("Variables")]
+    [SerializeField] private float playerSpeed = 7f;
+    [SerializeField] private float playerOffset = -45f;
+    [SerializeField] private float turnSpeed = 360f;
+    [SerializeField] private Vector2 movementVector2D;
+    [SerializeField] private Vector3 movementVector3D;
+
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementVector2D = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    private void Move()
+    {
+        if (movementVector2D == Vector2.zero)
+        {
+            return;
+        }
+
+        movementVector3D = new Vector3(movementVector2D.x, 0f, movementVector2D.y) * (Time.deltaTime * playerSpeed);
+
+        var matrix = Matrix4x4.Rotate(Quaternion.Euler(0f, playerOffset, 0f));
+        var skewedInput = matrix.MultiplyPoint3x4(movementVector3D);
+
+        rb.MovePosition(transform.position + skewedInput);
+    }
+
+    private void Look()
+    {
+        if (movementVector3D == Vector3.zero)
+        {
+            return;
+        }
+        
+        var relative = (rb.transform.position + movementVector3D) - rb.transform.position;
+        var rotation = Quaternion.LookRotation(relative, Vector3.up);
+        var offsetRotation = rotation * Quaternion.AngleAxis(-playerOffset, Vector3.down);
+
+        rb.transform.rotation = Quaternion.RotateTowards(rb.transform.rotation, offsetRotation, turnSpeed * Time.deltaTime);
+    }
+    
+    private void Update()
+    {
+        Move();
+        Look();
+    }
+}
