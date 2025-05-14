5823e1f1ac81edaff6c96ef6bd2f2d16daea0259
Sacha <S.Soulslasher@gmail.com>
2025-05-14 11:31:51 +0200

    [ADD] PlayerHealth

--- Assets/Script/Health/PlayerHealth.cs
+++ Assets/Script/Health/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour, IHealth
 {
@@ -9,40 +10,53 @@ public class PlayerHealth : MonoBehaviour, IHealth
     [SerializeField] private int maxHealth = 100;
     int IHealth._maxHealth { get => maxHealth; set => maxHealth = value; }
 
-
+    [SerializeField] Slider uiSlider;
+    Slider IHealth.slider { get; set; }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        if (currentHealth < 0)
+        if (currentHealth <= 0)
         {
             currentHealth = 0;
             OnDeath();
         }
+        UpdateSlider();
     }
 
     public void RegainHealth(int amount)
     {
         currentHealth += amount;
         if (currentHealth > maxHealth) currentHealth = maxHealth;
+        UpdateSlider();
     }
 
     public void OnDeath()
     {
-        Debug.Log(name = " Died.");
+        Debug.Log(gameObject.name = " Died.");
         throw new System.NotImplementedException();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        { 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             TakeDamage(10);
         }
 
         if (Input.GetKeyDown(KeyCode.V))
         {
             RegainHealth(10);
+        }
+    }
+
+    public void UpdateSlider()
+    {
+        if (uiSlider)
+        {
+            float value = (float)currentHealth / (float)maxHealth;
+            uiSlider.value = value;
+            print(value);
         }
     }
 }
